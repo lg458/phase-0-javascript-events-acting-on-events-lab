88889007bd4ceba95d79274fe2cdd18b68f656f0
Laura Gonzalez <lg458@cornell.edu>
2022-06-18 13:22:02 -0700

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Your code here
+const dodger = document.getElementById("dodger"); 
+
+//left movement function 
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+//left event listener
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+}); 
+
+//right movement function 
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0 && left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+} 
+
+//right event listener
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+}); 
